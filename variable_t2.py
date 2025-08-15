8b0c4380a80b09690be62d0f18ed5293ad5c6abc
marshaltech006-gif <marshal.tech006@gmail.com>
2025-08-15 11:07:23 +0530

    Initial commit

---
+++ variable_t2.py
@@ -0,0 +1,35 @@
+# Variables in Python
+first_name = 'Marshal'
+last_name = 'Tech'
+city = 'Mahabubabad'
+age = 20
+is_married = False
+skills = ['Python', 'Gaming']
+
+personal_info = {
+    'first_name': 'Marshal',
+    'last_name': 'Tech',
+    'city': 'Mahabubabad',
+    'age': 20,
+    'is_married': False,
+    'skills': ['Python', 'Gaming']
+}
+
+# Print variables
+print(f"First name is: {first_name}")
+print(f"Last name is: {last_name}")
+print(f"City is: {city}")
+print(f"Age is: {age}")
+print(f"Is married? {is_married}")
+print(f"My skills are: {skills}")
+print(f"My personal info is: {personal_info}")
+
+# Variable assignment in one line
+first_name, last_name, city, age, is_married, skills = 'Marshal', 'Tech', 'Mahabubabad', 20, False, ['Python', 'Gaming']
+
+print(f"First name is: {first_name}")
+print(f"Last name is: {last_name}")
+print(f"City is: {city}")
+print(f"Age is: {age}")
+print(f"Is married? {is_married}")
+print(f"My skills are: {skills}")
